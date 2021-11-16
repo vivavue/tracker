9003bb65180c8b94027681f426fb5f45a933877e
John Smith <john@mail.ru>
2021-11-16 07:56:33 +0400

    add RoleManager

--- app/Http/Controllers/RoleManager.php
+++ app/Http/Controllers/RoleManager.php
@@ -2,7 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Spatie\Permission\Models\Role;
+use Spatie\Permission\Models\Permission;
 
 class RoleManager extends Controller
 {
